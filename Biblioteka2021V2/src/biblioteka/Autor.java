163eaaff5fa72bbd70f65c13ca5cb5adb32d3cda
ttodinator <ttodinator@gmail.com>
2021-03-15 18:16:50 +0100

    Pocetna aplikacija

---
+++ Biblioteka2021V2/src/biblioteka/Autor.java
@@ -0,0 +1,89 @@
+package biblioteka;
+
+public class Autor {
+
+	private String ime;
+	
+
+	private String prezime;
+
+	public Autor() {
+		super();
+	}
+
+
+	public Autor(String ime, String prezime) {
+		super();
+		setIme(ime);
+		setPrezime(prezime);
+	}
+
+
+	public String getIme() {
+		return ime;
+	}
+	
+
+	public void setIme(String ime) {
+		if (ime == null)
+			throw new NullPointerException("Ime ne sme biti null");
+		
+		if (ime.length() < 2)
+			throw new RuntimeException("Ime mora imati vise od dva znaka");
+		
+		this.ime = ime;
+	}
+		
+
+	public String getPrezime() {
+		return prezime;
+	}
+	
+
+	public void setPrezime(String prezime) {
+		if (prezime == null)
+			throw new NullPointerException("Prezime ne sme biti null");
+		
+		if (prezime.length() < 2)
+			throw new RuntimeException("Prezime mora imati vise od dva znaka");
+		
+		this.prezime = prezime;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+		result = prime * result + ((prezime == null) ? 0 : prezime.hashCode());
+		return result;
+	}
+	
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		if (prezime == null) {
+			if (other.prezime != null)
+				return false;
+		} else if (!prezime.equals(other.prezime))
+			return false;
+		return true;
+	}
+}
---
+++ Biblioteka2021V2/src/biblioteka/Biblioteka.java
@@ -0,0 +1,49 @@
+package biblioteka;
+
+import java.util.LinkedList;
+
+import biblioteka.intefejs.BibliotekaInterface;
+
+public class Biblioteka implements BibliotekaInterface{
+	
+private LinkedList<Knjiga> knjige = new LinkedList<Knjiga>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+		
+		if (knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga vec postoji");
+		
+		knjige.add(knjiga);
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null || !knjige.contains(knjiga))
+			throw new RuntimeException("Knjiga ne sme biti null i mora postojati u biblioteci");
+		
+		knjige.remove(knjiga);
+	}
+
+	@Override
+	public LinkedList<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+		if (autor == null && isbn == null && naslov == null && izdavac == null)
+			throw new RuntimeException("Morate uneti bar jedan kriterijum pretrage");
+		
+		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
+		for(Knjiga knjiga: knjige)
+			if (knjiga.getNaslov().contains(naslov))
+				rezultat.add(knjiga);
+		
+		return rezultat;
+	}
+
+}
